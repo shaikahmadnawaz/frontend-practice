a1b7f01d94c2c1ea0e6da5121402d76c8aca31f3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-03 17:31:54 +0530

    closure exapmle

--- javascript/closure.js
+++ javascript/closure.js
@@ -1,3 +1,5 @@
+// a closure is a function that remembers and can access variables from its parent scope, even after the parent function has finished executing.
+
 function outerFunction() {
   var outerVariable = "Hello";
 
@@ -10,3 +12,16 @@ function outerFunction() {
 
 var closure = outerFunction();
 closure(); // Output: Hello
+
+// Callbacks and Event Handling:
+// Closures are commonly used for managing callbacks and event handling. When a function with a callback is executed, the callback retains access to variables in its parent scope, even after the parent function has completed execution.
+
+function onClick(elementId) {
+  var element = document.getElementById(elementId);
+
+  element.addEventListener("click", function () {
+    console.log("Element with ID", elementId, "was clicked.");
+  });
+}
+
+onClick("myButton");
