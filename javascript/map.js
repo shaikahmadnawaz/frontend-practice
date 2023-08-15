0cf3f99ea4689515de0531ed233a7d38257ce2d9
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-08-15 23:52:13 +0530

    revised map & reduce

--- javascript/map.js
+++ javascript/map.js
@@ -1,7 +1,18 @@
+// In JavaScript, the map method is a built-in function available for arrays. It's used to create a new array by applying a provided function to each element of the original array. The purpose of the map method is to transform the elements of an array into a new set of values without changing the original array.
+
 const numbers = [1, 2, 3, 4, 5];
 
-const multipliedByTwo = numbers.map(function (number) {
+const multipliedByTwo = numbers.map((number) => {
   return number * 2;
 });
 
 console.log(multipliedByTwo); // Output: [2, 4, 6, 8, 10]
+
+// chaining
+
+const chainEg = numbers
+  .map((number) => number * 2)
+  .map((number) => number + 1)
+  .filter((number) => number > 5);
+
+console.log(chainEg); // Output: [7, 9, 11]
--- javascript/reduce.js
+++ javascript/reduce.js
@@ -1,7 +1,33 @@
+// In JavaScript, the reduce method is a built-in function available for arrays. It's used to "reduce" the elements of an array into a single value by repeatedly applying a provided function in a cumulative manner. The reduce method is useful for operations like summing up values, calculating averages, or any other operation that involves accumulating values.
+
 const numbers = [1, 2, 3, 4, 5];
 
-const sum = numbers.reduce(function (accumulator, number) {
+const initialValue = 0;
+const sum = numbers.reduce((accumulator, number) => {
   return accumulator + number;
-}, 0);
+}, initialValue);
 
 console.log(sum); // Output: 15
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 999,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 5999,
+  },
+  {
+    itemName: "data science course",
+    price: 12999,
+  },
+];
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(priceToPay); // Output: 22596
