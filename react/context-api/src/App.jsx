5d594d59e3f3a74574eca0d33063748e574e8dfb
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-09-12 21:16:15 +0530

    revised ContextAPI

---
+++ react/context-api/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
